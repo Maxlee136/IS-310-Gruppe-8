ecdb90a06062a7627f4684653e53ff7fcb70edfe
andreas morkesdal <andreas.morkesdal@gmail.com>
2024-04-25 15:20:36 +0200

    Lagt til profilbilde + info

--- cv-frontend/src/pages/Homepage.tsx
+++ cv-frontend/src/pages/Homepage.tsx
@@ -80,17 +80,17 @@ const HomePage: React.FC = () => {
                                 className="w-full bg-white rounded-lg shadow-lg overflow-hidden flex flex-col md:flex-row">
                                 <div className="w-full md:w-2/5 h-80">
                                     <img className="object-center object-cover w-full h-full"
-                                         src="https://scontent.fosl3-2.fna.fbcdn.net/v/t39.30808-6/384114230_18054013399466160_7478538626211862689_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ermsxSkjrZIAb5hppLf&_nc_ht=scontent.fosl3-2.fna&oh=00_AfCqx68UHeCmSlc2EHGXINqXK5n4qK0NrTfFggzfpWIURw&oe=663019A6"
+                                         src="https://media.licdn.com/dms/image/D4E03AQHMFORjvQHEkw/profile-displayphoto-shrink_400_400/0/1712234915388?e=1719446400&v=beta&t=rv5tH6m_TVXBEmS1pLy_tB8BkH4BHWEdpbc0zv6jwCY"
                                          alt="photo"></img>
                                 </div>
                                 <div className="w-full md:w-3/5 text-left p-6 md:p-4 space-y-2">
-                                    <p className="text-xl text-gray-700 font-bold">Max Lee</p>
+                                    <p className="text-xl text-gray-700 font-bold">Andreas Mørkesdal</p>
                                     <p className="text-base text-gray-400 font-normal">IT Student</p>
                                     <p className="text-base leading-relaxed text-gray-500 font-normal">Hei! Mitt navn er
-                                        Max Lee. Jeg er It student med en passion for programmering. Trykke på kortet
+                                        Andreas Mørkesdal. Jeg er IT student med fagbrev som IKT-servicemedarbeider. Trykk på kortet
                                         mitt for å lære mer om meg</p>
                                     <div className="flex justify-start space-x-2">
-                                        <a href="https://www.facebook.com/profile.php?id=100006561961710&locale=nb_NO"
+                                        <a href="https://www.facebook.com/profile.php?id=100008480328278"
                                            className="text-gray-500 hover:text-gray-600">
                                             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor"
                                                  viewBox="0 0 24 24">
@@ -99,24 +99,7 @@ const HomePage: React.FC = () => {
                                                       clip-rule="evenodd"></path>
                                             </svg>
                                         </a>
-                                        <a href="https://www.instagram.com/leemax02/"
-                                           className="text-gray-500 hover:text-gray-600">
-                                            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor"
-                                                 viewBox="0 0 24 24">
-                                                <path fill-rule="evenodd"
-                                                      d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                                                      clip-rule="evenodd"></path>
-                                            </svg>
-                                        </a>
-                                        <a href="https://twitter.com/Max02Lee"
-                                           className="text-gray-500 hover:text-gray-600">
-                                            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor"
-                                                 viewBox="0 0 24 24">
-                                                <path fill-rule="evenodd"
-                                                      d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
-                                            </svg>
-                                        </a>
-                                        <a href="https://github.com/Maxlee136"
+                                        <a href="https://github.com/Andreassm99"
                                            className="text-gray-500 hover:text-gray-600">
                                             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor"
                                                  viewBox="0 0 24 24">
