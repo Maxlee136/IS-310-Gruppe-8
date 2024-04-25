8ed267d1da7a6b1bf3b3652fb5f11fd2032e61e8
andreas morkesdal <andreas.morkesdal@gmail.com>
2024-04-25 16:11:03 +0200

    fiks av overskrift

--- cv-frontend/src/pages/Homepage.tsx
+++ cv-frontend/src/pages/Homepage.tsx
@@ -14,7 +14,7 @@ const HomePage: React.FC = () => {
             <section  className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
                 <div  className="text-center pb-12">
                     <h2 className="text-base font-bold text-indigo-600">
-                        Gruppe søknads side
+                        Søknad til praksis
                     </h2>
                     <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
                         Sjekk ut de forskjellige medlemmene i gruppen
