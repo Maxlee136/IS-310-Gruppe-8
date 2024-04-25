9066debe9cb7ad940f6edf46a21db5a0c3c2794e
Maxlee136@gmail.com <Maxlee136>
2024-04-25 14:53:36 +0200

    i have done so that all the cards line up

--- cv-frontend/src/pages/ProfilePage.tsx
+++ cv-frontend/src/pages/ProfilePage.tsx
@@ -15,9 +15,16 @@ const ProfilePage: React.FC = () => {
 
     return (
         <div>
-            <h1>{member.name}</h1>
-            <p>{member.role}</p>
-            <a href={member.resumeLink} download>Download Resume</a>
+            <div className="container mx-auto mt-8 md:mt-0 md:space-x-10 md:grid grid-cols-3 justify-center md:py-40">
+                <div className="grid justify-center items-center order-1 col-span-1">
+                    <img className="lg:h-80 md:h-64 h-40 rounded-full" src="https://scontent.fosl3-2.fna.fbcdn.net/v/t39.30808-6/384114230_18054013399466160_7478538626211862689_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ermsxSkjrZIAb5hppLf&_nc_ht=scontent.fosl3-2.fna&oh=00_AfCqx68UHeCmSlc2EHGXINqXK5n4qK0NrTfFggzfpWIURw&oe=663019A6" alt="" />
+                </div>
+                <div className="mt-8 md:mt-0 lg:justify-end col-span-2">
+                    <h1 className="text-4xl text-gray-800 text-center md:text-left font-bold mb-6">Hei, mitt navn er Max Lee</h1>
+                    <p className="text-xl text-gray-800 text-center md:text-left">Jeg er en it Student, med erfaring innenfor React og Java Spingboot. Jeg er veldig glad i å programmere og jobber med en Saas på fritiden</p>
+                    <button className="block mt-8 mx-auto md:mx-0 text-2xl py-3 px-6 text-red-50 font-semibold rounded bg-red-400">Last ned CV</button>
+                </div>
+            </div>
         </div>
     );
 };
