f3583cb9cbdd4ada821352fc2350451bea091eb3
Maxlee136@gmail.com <Maxlee136>
2024-08-23 18:40:31 +0200

    endret linkedin knapp og tekst om meg selv

--- cv-frontend/src/pages/ProfilePage.tsx
+++ cv-frontend/src/pages/ProfilePage.tsx
@@ -14,9 +14,13 @@ const ProfilePage: React.FC = () => {
                     id: 1,
                     name: 'Max Lee',
                     role: 'Student',
-                    image: 'https://media.licdn.com/dms/image/D4D03AQFPOKdKGlBA0w/profile-displayphoto-shrink_800_800/0/1708106515253?e=1719446400&v=beta&t=0a3tBJ9Ea7YB1EKeP2daJmiLrQX_qvjl9igbSBeh--E',
-                    LinkToCV: 'https://drive.google.com/file/d/1siBjtgegJcGXDxzm8MEJ8u1zNjgSE_bq/view?usp=drive_link',
-                    description: 'Jeg er en blid student med en passion for programmering. Trives best med fullstack hvor jeg har erfaring med React og Java Springboot. Klikk på kortet mitt for å lære mer om meg'
+                    image: '/Max.jpeg',
+                    LinkToLinkedIn: 'https://www.linkedin.com/in/max-lee-257182221/',
+                    description:
+                        'Jeg er en blid student med en passion for fullstack utvikling, mine ferdigheter er hovedsakelig innen Java og React TSX. ' +
+                        'Jeg er gründer av to selskaper, hovedsakelig et SAAS selskap kallt Enthemed som er laget for nettbutikker i shopify sitt økosystem' +
+                        ''
+
                 },
                 {
                     id: 2,
@@ -31,7 +35,7 @@ const ProfilePage: React.FC = () => {
                     role: 'Student',
                     image: 'https://media.licdn.com/dms/image/D4E03AQHMFORjvQHEkw/profile-displayphoto-shrink_800_800/0/1712234915388?e=1719446400&v=beta&t=I2PmYMkq3V8k-0IeWMLU49x0snzVLo4Z0p2B3LdOVvQ',
                     description: 'Another profile description here.',
-                    LinkToCV: 'https://drive.google.com/file/d/1siBjtgegJcGXDxzm8MEJ8u1zNjgSE_bq/view?usp=drive_link'
+                    LinkToLinkedIn: 'https://drive.google.com/file/d/1siBjtgegJcGXDxzm8MEJ8u1zNjgSE_bq/view?usp=drive_link'
                 }
             ];
             const selectedMember = members.find(m => m.id.toString() === id);
@@ -52,7 +56,7 @@ const ProfilePage: React.FC = () => {
                 <div className="mt-8 md:mt-0 lg:justify-end col-span-2">
                     <h1 className="text-4xl text-gray-800 text-center md:text-left font-bold mb-6">{member.name}</h1>
                     <p className="text-xl text-gray-800 text-center md:text-left">{member.description}</p>
-                    <button onClick={() => window.open(member.LinkToCV, '_blank')} className="block mt-8 mx-auto md:mx-0 text-2xl py-3 px-6 text-red-50 font-semibold rounded bg-red-400">Last ned CV</button>
+                    <button onClick={() => window.open(member.LinkToLinkedIn, '_blank')} className="block mt-8 mx-auto md:mx-0 text-2xl py-3 px-6 text-red-50 font-semibold rounded bg-red-400">LinkedIn profil</button>
                 </div>
             </div>
         </div>
